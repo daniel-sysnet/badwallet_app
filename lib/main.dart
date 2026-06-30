c68639866d1fbc0c4db68da7371fb197979177bf
daniel-sysnet <danielmolombo96@gmail.com>
2026-06-30 16:04:33 +0000

    Ajout du point d'entrée de l'application

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'core/theme/app_theme.dart';
 import 'features/auth/providers/auth_provider.dart';
+import 'features/dashboard/providers/wallet_provider.dart';
+import 'features/history/providers/transaction_provider.dart';
+import 'features/transfers/providers/transfer_provider.dart';
+import 'features/bills/providers/bills_provider.dart';
 import 'features/auth/screens/splash_screen.dart';
 
 void main() {
@@ -17,6 +20,10 @@ class BadWalletApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => WalletProvider()),
+        ChangeNotifierProvider(create: (_) => TransactionProvider()),
+        ChangeNotifierProvider(create: (_) => TransferProvider()),
+        ChangeNotifierProvider(create: (_) => BillsProvider()),
       ],
       child: MaterialApp(
         title: 'BadWallet',
